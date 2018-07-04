d90dbe21be90f4258c6d765b97eaaff47f53b1a4
Ryan <ryangohjq@gmail.com>
2018-07-04 11:19:42 +0800

    Added back dist

---
+++ dist/config.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const constants_1 = require("./constants");
+const utils_1 = require("./utils");
+const path_1 = __importDefault(require("path"));
+exports.getConfig = () => {
+    const { DEBUG, SECURE, AUTH_MODE, LOCAL_PORT, GATEWAY_1_HOST, GATEWAY_1_PORT, GATEWAY_1_TYPE, GATEWAY_1_URL_PREFIX, GATEWAY_1_APP_ID, GATEWAY_1_SECRET, GATEWAY_1_KEY_STRING, GATEWAY_1_KEY_FILE, GATEWAY_1_PASSPHRASE, GATEWAY_2_HOST, GATEWAY_2_PORT, GATEWAY_2_TYPE, GATEWAY_2_URL_PREFIX, GATEWAY_2_APP_ID, GATEWAY_2_SECRET, GATEWAY_2_KEY_STRING, GATEWAY_2_KEY_FILE, GATEWAY_2_PASSPHRASE, } = process.env;
+    return {
+        debug: utils_1.isTruthy(DEBUG),
+        secure: utils_1.isTruthy(SECURE),
+        mode: AUTH_MODE || constants_1.DEFAULT_MODE,
+        localPort: LOCAL_PORT,
+        gateway1Host: GATEWAY_1_HOST,
+        gateway1Port: GATEWAY_1_PORT,
+        gateway1Type: GATEWAY_1_TYPE,
+        gateway1UrlPrefix: GATEWAY_1_URL_PREFIX,
+        gateway1AppId: GATEWAY_1_APP_ID,
+        gateway1Secret: GATEWAY_1_SECRET ? GATEWAY_1_SECRET : undefined,
+        gateway1KeyString: GATEWAY_1_KEY_STRING ? GATEWAY_1_KEY_STRING : undefined,
+        gateway1KeyFile: GATEWAY_1_KEY_FILE ? path_1.default.join(__dirname, '..', GATEWAY_1_KEY_FILE) : undefined,
+        gateway1Passphrase: GATEWAY_1_PASSPHRASE ? GATEWAY_1_PASSPHRASE : undefined,
+        gateway2Host: GATEWAY_2_HOST,
+        gateway2Port: GATEWAY_2_PORT,
+        gateway2Type: GATEWAY_2_TYPE,
+        gateway2UrlPrefix: GATEWAY_2_URL_PREFIX,
+        gateway2AppId: GATEWAY_2_APP_ID,
+        gateway2Secret: GATEWAY_2_SECRET ? GATEWAY_2_SECRET : undefined,
+        gateway2KeyString: GATEWAY_2_KEY_STRING ? GATEWAY_2_KEY_STRING : undefined,
+        gateway2KeyFile: GATEWAY_2_KEY_FILE ? path_1.default.join(__dirname, '..', GATEWAY_2_KEY_FILE) : undefined,
+        gateway2Passphrase: GATEWAY_2_PASSPHRASE ? GATEWAY_2_PASSPHRASE : undefined,
+    };
+};
+exports.printConfig = () => {
+    const config = exports.getConfig();
+    console.log(`Debug Mode: ${config.debug}`);
+    console.log(`Secure Mode: ${config.secure}`);
+    console.log(`Auth Mode: ${config.mode}`);
+    console.log(`Server listening on port ${config.localPort}`);
+};
+exports.default = exports.getConfig;
+//# sourceMappingURL=config.js.map
---
+++ dist/constants.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MODE = {
+    REWRITE: 'REWRITE',
+    APPEND: 'APPEND',
+};
+exports.AUTH_PREFIX = {
+    L1: {
+        INTERNAL: 'apex_l1_ig',
+        EXTERNAL: 'apex_l1_eg',
+    },
+    L2: {
+        INTERNAL: 'apex_l2_ig',
+        EXTERNAL: 'apex_l2_eg',
+    }
+};
+exports.DEFAULT_MODE = exports.MODE.REWRITE;
+//# sourceMappingURL=constants.js.map

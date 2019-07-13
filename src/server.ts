11cb46a4ca1ac1dd77da258153ec89488234bab8
Ryan <ryangohjq@gmail.com>
2019-07-13 15:28:59 +0800

    Changed targetpath to not include gateway2url if gatewayIsSingle

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import connect from 'connect';
-import {config as populateProcessEnv} from 'dotenv';
+import { config as populateProcessEnv } from 'dotenv';
 import http from 'http';
 import httpProxy from 'http-proxy';
 import httpsProxyAgent from 'https-proxy-agent';
 import path from 'path';
 import streamArray = require('stream-array');
-import {getConfig, printConfig} from './config';
-import {proxyHandler} from './handler';
+import { getConfig, printConfig } from './config';
+import { proxyHandler } from './handler';
 
 // Populate environment variable from dotenv file if applicable
 try {
-  populateProcessEnv({path: path.join(__dirname, '../.env')});
+  populateProcessEnv({ path: path.join(__dirname, '../.env') });
 } catch (ex) {
   console.log('Unable to load dotenv', ex);
 }
@@ -20,29 +20,34 @@ try {
 const config = getConfig();
 const httpProxyValue = config.customHttpProxy || config.httpProxy;
 const httpProxyAgent = httpProxyValue ? new httpsProxyAgent(httpProxyValue) : false;
-const proxyWithAgentOptions = { toProxy: config.toProxy, agent: httpProxyAgent};
+const proxyWithAgentOptions = { toProxy: config.toProxy, agent: httpProxyAgent };
+const basePath = `https://${config.gateway1Host}:${config.gateway1Port}/${config.gateway1UrlPrefix}`;
+let targetPath = basePath;
+if (config.gatewayIsSingle) {
+  targetPath += `/${config.gateway2UrlPrefix}`
+}
 const proxyOptions = Object.assign({
   // tslint:disable-next-line:max-line-length
-  target: `https://${config.gateway1Host}:${config.gateway1Port}/${config.gateway1UrlPrefix}/${config.gateway2UrlPrefix}`,
+  target: targetPath,
   secure: config.secure,
   timeout: config.timeout,
   proxy_timeout: config.proxyTimeout,
 }, config.useProxyAgent && proxyWithAgentOptions);
 
-console.log({proxyOptions});
+console.log({ proxyOptions });
 const proxy = httpProxy.createProxyServer(proxyOptions);
 
 proxy.on('proxyReq', (proxyReq: http.ClientRequest, req: http.IncomingMessage) => {
-  if(config.debug) {
-    console.log(Object.assign({type: 'on.proxyReq'}, {headers:proxyReq.getHeaders()}));
+  if (config.debug) {
+    console.log(Object.assign({ type: 'on.proxyReq' }, { headers: proxyReq.getHeaders() }));
   }
   proxyHandler(proxyReq, req, config);
 });
 
 proxy.on('proxyRes', (proxyRes: http.IncomingMessage) => {
-  if(config.debug){
+  if (config.debug) {
     console.info('[proxyRes debug]');
-    console.log(Object.assign({type: 'on.proxyRes'}, {headers: proxyRes.headers}, {
+    console.log(Object.assign({ type: 'on.proxyRes' }, { headers: proxyRes.headers }, {
       statusCode: proxyRes.statusCode,
       statusMessage: proxyRes.statusMessage,
       httpVersion: proxyRes.httpVersion,
@@ -71,8 +76,8 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(bodyParser.json({limit: config.bodyLimitSize}));
-app.use(bodyParser.urlencoded({extended: true, limit: config.bodyLimitSize}));
+app.use(bodyParser.json({ limit: config.bodyLimitSize }));
+app.use(bodyParser.urlencoded({ extended: true, limit: config.bodyLimitSize }));
 app.use((req, res, next) => {
   const webProxyOptions: httpProxy.ServerOptions = {
     changeOrigin: true,
@@ -85,7 +90,7 @@ app.use((req, res, next) => {
   }
   proxy.web(req, res, webProxyOptions, next);
 });
-app.use(compression({filter: () => true}));
+app.use(compression({ filter: () => true }));
 app.use((err, _req, res, next) => {
   if (err) {
     res.setHeader('Content-Type', 'application/json');
